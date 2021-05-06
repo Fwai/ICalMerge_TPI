552eb6b452e93df43c449a24e7742b5b4f559282
Fwai <47890868+Fwai@users.noreply.github.com>
2021-05-06 10:19:00 +0200

    Ajout de la planification initiale + Arborescence fichiers +  Interface graphique

---
+++ ICalMerge/ICalMerge/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace ICalMerge
+{
+    partial class formIcal
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pnlSources = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblFusion = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnFusion = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.pnlSources.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(304, 15);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 34);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Ajouter une source";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Location = new System.Drawing.Point(-2, -3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(769, 65);
+            this.panel2.TabIndex = 8;
+            // 
+            // pnlSources
+            // 
+            this.pnlSources.Controls.Add(this.label2);
+            this.pnlSources.Controls.Add(this.textBox2);
+            this.pnlSources.Controls.Add(this.label4);
+            this.pnlSources.Controls.Add(this.button3);
+            this.pnlSources.Controls.Add(this.label1);
+            this.pnlSources.Controls.Add(this.textBox1);
+            this.pnlSources.Controls.Add(this.label3);
+            this.pnlSources.Controls.Add(this.button1);
+            this.pnlSources.Location = new System.Drawing.Point(12, 83);
+            this.pnlSources.Name = "pnlSources";
+            this.pnlSources.Size = new System.Drawing.Size(755, 68);
+            this.pnlSources.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(0, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Source 2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(74, 31);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(465, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(626, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "OK : 5 évènements";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(545, 31);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "parcourir";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Source 1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(74, 1);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(465, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(626, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "OK : 5 évènements";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(545, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "parcourir";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblFusion);
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Controls.Add(this.btnFusion);
+            this.panel1.Location = new System.Drawing.Point(12, 157);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(743, 128);
+            this.panel1.TabIndex = 8;
+            // 
+            // lblFusion
+            // 
+            this.lblFusion.AutoSize = true;
+            this.lblFusion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFusion.Location = new System.Drawing.Point(237, 102);
+            this.lblFusion.Name = "lblFusion";
+            this.lblFusion.Size = new System.Drawing.Size(276, 16);
+            this.lblFusion.TabIndex = 2;
+            this.lblFusion.Text = "Fusion terminée avec succès : X événements";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(215, 66);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(310, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // btnFusion
+            // 
+            this.btnFusion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFusion.Location = new System.Drawing.Point(215, 22);
+            this.btnFusion.Name = "btnFusion";
+            this.btnFusion.Size = new System.Drawing.Size(310, 38);
+            this.btnFusion.TabIndex = 0;
+            this.btnFusion.Text = "Fusionner";
+            this.btnFusion.UseVisualStyleBackColor = true;
+            // 
+            // formIcal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 297);
+            this.Controls.Add(this.pnlSources);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "formIcal";
+            this.ShowIcon = false;
+            this.Text = "IcalMerge";
+            this.panel2.ResumeLayout(false);
+            this.pnlSources.ResumeLayout(false);
+            this.pnlSources.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel pnlSources;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblFusion;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnFusion;
+    }
+}
+
